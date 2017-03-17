f7d5a78fa8c2ef7acc08741cb25e79a4951a731a
Adrian Daras <getpingui@gmail.com>
2017-03-17 11:58:00 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,50 @@
 class ApplicationController < Sinatra::Base
   configure do
+    enable :method_override
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  get '/' do
+
+  get '/recipes' do
+    @recipes = Recipe.all
     erb :index
+  end
+
+  get "/recipes/new" do
+    erb :new_recipe
+  end
+
+  get "/recipes/:id" do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  get "/recipes/:id/edit" do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  post "/recipes" do
+    recipe = Recipe.new(params)
+    recipe.save
+
+    redirect "/recipes/#{recipe.id}"
+  end
+
+  patch "/recipes/:id" do
+    recipe = Recipe.find(params[:id])
+    recipe.name = params[:name]
+    recipe.ingredients = params[:ingredients]
+    recipe.cook_time = params[:cook_time]
+    recipe.save
+
+    redirect "/recipes/#{recipe.id}"
+  end
+
+  delete "/recipes/:id/delete" do
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+
+    redirect "/recipes"
   end
 end
