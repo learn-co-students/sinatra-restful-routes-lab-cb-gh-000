be760e4c187890eba6d46625685dbf93e2a5db66
Robert Wayne <robertjwayne@gmail.com>
2017-05-10 16:07:11 -0400

    Add routes that will be required (without actions)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,30 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   get '/' do
     erb :index
   end
+
+  get '/recipes' do
+  end
+
+  post '/recipes' do
+  end
+
+  get '/recipes/new' do
+  end
+
+  get '/recipes/:id/edit' do
+  end
+
+  get '/recipes/:id' do
+  end
+
+  patch '/recipes/:id' do
+  end
+
+  delete '/recipes/:id/delete' do
+  end
+
 end
