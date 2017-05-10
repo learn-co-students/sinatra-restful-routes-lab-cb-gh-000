1788c26ebad725dcab50609d09c2c676421b2f70
Robert Wayne <robertjwayne@gmail.com>
2017-05-10 15:35:49 -0400

    Migrate Recipes table to database

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+Recipe.create(:name => "First recipe", :ingredients => "Ingredients for first recipe", :cook_time => "Cook time for first recipe")
+
+Recipe.create(:name => "Second recipe", :ingredients => "Ingredients for second recipe", :cook_time => "Cook time for second recipe")
+
+Recipe.create(:name => "Third recipe", :ingredients => "Ingredients for third recipe", :cook_time => "Cook time for third recipe")
+
+Recipe.create(:name => "Fourth recipe", :ingredients => "Ingredients for fourth recipe", :cook_time => "Cook time for fourth recipe")
