868444535b4d201830fe780600ed2fbfe1794a4e
Quinn Dizon <qdizon@gmail.com>
2018-08-18 10:53:09 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,10 @@ class ApplicationController < Sinatra::Base
     erb :recipes
   end
 
+  get '/recipes/new' do
+    erb :new
+  end
+
   get '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
 
@@ -36,7 +40,13 @@ class ApplicationController < Sinatra::Base
     recipe.ingredients = params[:ingredients]
     recipe.cook_time = params[:cook_time]
     recipe.save
-    redirect to '/recipes'
+    redirect to "/recipes/#{recipe.id}"
+  end
+
+  # CREATE
+  post '/recipes' do
+    recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect to "/recipes/#{recipe.id}"
   end
 
 
